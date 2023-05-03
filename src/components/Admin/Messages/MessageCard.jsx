8616ef7afcb3828db243b630ac44bb8515708ea4
Nikodem1290494 <nikodemtyc2001@interia.pl>
2023-05-03 18:06:45 +0200

    Add: display messages on admin panel

---
+++ src/components/Admin/Messages/MessageCard.jsx
@@ -0,0 +1,23 @@
+import { Card, Text, Flex, Box } from "@chakra-ui/react";
+
+export default function MessageCard({ message }) {
+    const date = new Date(message.created_at);
+    const formattedDate = ${date.getDate().toString().padStart(2, '0')}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getFullYear().toString()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')};
+
+    return (
+        <Card w={'60%'} minH={'20vh'}>
+            <Flex justifyContent={'space-between'} p={'1rem'} gap={'20px'}>
+                <Flex direction={'column'} >
+                    <Text>Imię: {message.firstName}</Text>
+                    <Text>Nazwisko: {message.lastName}</Text>
+                    <Text>Email: {message.email}</Text>
+                    <Text>Numer telefonu: {message.phoneNumber}</Text>
+                    <Text>Data: {formattedDate}</Text>
+                </Flex>
+                <Box  flex={'1'}>
+                    <Text>{message.message}</Text>
+                </Box>
+            </Flex>
+        </Card>
+    )
+}
