5fedb2138c280bfd60d5276d6a22aa1080c17b81
KamilProjekt <kamilosek2000@o2.pl>
2023-06-22 01:53:33 +0200

    Poprawa walidacji email w kontakcie

--- src/components/User/ContactForm/ContactForm.jsx
+++ src/components/User/ContactForm/ContactForm.jsx
@@ -14,12 +14,12 @@ import SectionHeader from "@/components/UI/SectionHeader";
 import { useSession } from "@supabase/auth-helpers-react";
 
 export default function ContactForm({ onSubmit, profile }) {
-  const session = useSession()
+  const session = useSession();
   const {
     handleSubmit,
     register,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
 
   const submitHandler = (data) => {
@@ -29,9 +29,7 @@ export default function ContactForm({ onSubmit, profile }) {
 
   return (
     <>
- 
       <Box minH={"75vh"} mt={"7rem"}>
-        
         <Flex
           w={{ base: "90%", md: "70%", lg: "50%", xl: "40%", "2xl": "30%" }}
           margin={"5rem auto"}
@@ -46,33 +44,71 @@ export default function ContactForm({ onSubmit, profile }) {
             textAlign={"center"}
           >
             <FormLabel>Imię</FormLabel>
-            <Input type="text" {...register("firstName", { required: true, minLength:2, maxLength:18 })} defaultValue={(session?profile?.firstName : '')} />
+            <Input
+              type="text"
+              {...register("firstName", {
+                required: true,
+                minLength: 2,
+                maxLength: 18,
+              })}
+              defaultValue={session ? profile?.firstName : ""}
+            />
             {errors.firstName && (
               <Text color={"yellow.200"}>To pole jest wymagane</Text>
             )}
-            {errors.firstName && errors.firstName.type==="minLength" &&  (
-              <Text color={"yellow.200"}>Imie jest za krótkie (min 2 znaki)</Text>
+            {errors.firstName && errors.firstName.type === "minLength" && (
+              <Text color={"yellow.200"}>
+                Imie jest za krótkie (min 2 znaki)
+              </Text>
             )}
-            {errors.firstName && errors.firstName.type==="maxLength" &&  (
-              <Text color={"yellow.200"}>Imie jest za długie (max 18 znaków)</Text>
+            {errors.firstName && errors.firstName.type === "maxLength" && (
+              <Text color={"yellow.200"}>
+                Imie jest za długie (max 18 znaków)
+              </Text>
             )}
 
             <FormLabel>Nazwisko</FormLabel>
-            <Input type="text" {...register("lastName", { required: true, minLength:2, maxLength:18 })} defaultValue={(session?profile?.lastName : '')} />
+            <Input
+              type="text"
+              {...register("lastName", {
+                required: true,
+                minLength: 2,
+                maxLength: 18,
+              })}
+              defaultValue={session ? profile?.lastName : ""}
+            />
             {errors.lastName && (
               <Text color={"yellow.200"}>To pole jest wymagane</Text>
             )}
-            {errors.lastName && errors.lastName.type==="minLength" &&  (
-              <Text color={"yellow.200"}>Nazwisko jest za krótkie (min 2 znaki)</Text>
+            {errors.lastName && errors.lastName.type === "minLength" && (
+              <Text color={"yellow.200"}>
+                Nazwisko jest za krótkie (min 2 znaki)
+              </Text>
             )}
-            {errors.lastName && errors.lastName.type==="maxLength" &&  (
-              <Text color={"yellow.200"}>Nazwisko jest za długie (max 18 znaków)</Text>
+            {errors.lastName && errors.lastName.type === "maxLength" && (
+              <Text color={"yellow.200"}>
+                Nazwisko jest za długie (max 18 znaków)
+              </Text>
             )}
 
             <FormLabel>E-mail</FormLabel>
-            <Input type="email" {...register("email", { required: true })} defaultValue={(session?profile?.email : '')} />
+            <Input
+              type="email"
+              {...register("email", {
+                required: "To pole jest wymagane",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Niepoprawny adres e-mail",
+                },
+              })}
+              defaultValue={session ? profile?.email : ""}
+            />
             {errors.email && (
-              <Text color={"yellow.200"}>To pole jest wymagane</Text>
+              <Text color={"yellow.200"}>
+                {errors.email.type === "pattern"
+                  ? "Niepoprawny adres e-mail"
+                  : errors.email.message}
+              </Text>
             )}
 
             <FormLabel>Numer Telefonu</FormLabel>
@@ -83,7 +119,7 @@ export default function ContactForm({ onSubmit, profile }) {
                 minLength: 9,
                 maxLength: 9,
               })}
-              defaultValue={(session?profile?.phoneNumber : '')}
+              defaultValue={session ? profile?.phoneNumber : ""}
             />
             {errors.phoneNumber && errors.phoneNumber.type === "required" && (
               <Text color={"yellow.200"}>To pole jest wymagane</Text>
@@ -105,9 +141,7 @@ export default function ContactForm({ onSubmit, profile }) {
               <Text color={"yellow.200"}>To pole jest wymagane</Text>
             )}
             {errors.message && errors.message.type === "maxLength" && (
-              <Text color={"yellow.200"}>
-                Wiadomość jest za długa(max 300 znaków)
-              </Text>
+              <Text color={"yellow.200"}>Maksymalnie 300 znaków</Text>
             )}
 
             <Button
