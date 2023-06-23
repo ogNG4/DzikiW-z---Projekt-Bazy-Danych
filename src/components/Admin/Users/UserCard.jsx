db39219f158c4893e8c2f8bcbbd8c39ba082ba5d
KamilProjekt <kamilosek2000@o2.pl>
2023-06-23 14:33:59 +0200

    Poprawka

--- src/components/Admin/Users/UserCard.jsx
+++ src/components/Admin/Users/UserCard.jsx
@@ -3,7 +3,7 @@ import { Card, Flex, HStack, Text } from "@chakra-ui/react";
 
 export default function UserCard({profile}){
     return(
-        <Card w={'45%'} h={'max-content'} p={'1rem'} justifyContent={'space-between'}>
+        <Card w={'60%'} h={'max-content'} p={'1rem'} justifyContent={'space-between'}>
             <Flex  fontSize={'2xl'} gap={'3rem'}>
                 <Flex direction={'column'} gap={'1rem'} fontWeight={'bold'}>
                     <Text>Imię:</Text>
